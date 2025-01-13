0d520e6a4124cdb68b74a58d756ea7f0d4beb9ed
Kraken <krakeen99@gmail.com>
2025-01-13 10:35:10 +0200

    Add files via upload

--- src/main/java/ee/spring/materials/models/Product.java
+++ src/main/java/ee/spring/materials/models/Product.java
@@ -1,17 +1,52 @@
 package ee.spring.materials.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class Product{
+public class Product {
     private int id;
     private String name;
     private double price;
     private boolean active;
+
+    // Constructor
+    public Product(int id, String name, double price, boolean active) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.active = active;
+    }
+
+    // No-argument constructor
+    public Product() {}
+
+    // Getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
 }
